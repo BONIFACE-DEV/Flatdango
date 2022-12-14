f165747b058a13919c107e8631a405e52f37aaa5
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2022-12-14 09:44:24 +0300

    comments

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
+//base url
 const baseUrl = "http://localhost:3000/films";
+//fetching movies
 function fetchMovies(){
     fetch(baseUrl)
         .then((response) => response.json())
         .then((data) =>{
             data.forEach((films) =>{
-                let li = document.createElement("li");
-                li.textContent = films.title;
-                li.addEventListener("click",
-                (e)=>{
-                  let buttonContent =
-                  document.querySelector("button#buy-ticket")
-                  buttonContent.textContent = "Buy Tickets"
-                    let title =
-                 document.getElementById("movie-title");
-                    title.textContent =
-                films.title;
-                let img =
-                document.getElementById("movie-poster");
-                   img.src =
-                films.poster;
-                   let showTime =
-                document.getElementById("showtime");
-                  showTime.textContent =
-                films.showtime;
-                let runTime =
-                document.getElementById("runtime");
-                  runTime.textContent =
-                `${films.runtime} Minutes`;
-                let tickets =
-              document.querySelector("div#ticket-counter");
-                    tickets.textContent = films["capacity"] - films["tickets_sold"]
-                })
-                document.querySelector("ul#films").appendChild(li)
+              //creating li element
+    let li = document.createElement("li");
+    li.textContent = films.title;
+    li.addEventListener("click",
+    (e)=>{
+    let buttonContent = document.querySelector("button#buy-ticket")
+    buttonContent.textContent = "Buy Tickets"
+    let title =
+    document.getElementById("movie-title");
+    title.textContent =films.title;
+    // loading poster image 
+      let img =document.getElementById("movie-poster");
+      img.src =films.poster;
+      let showTime = document.getElementById("showtime");
+    //show time and runtime
+      showTime.textContent = films.showtime;
+      let runTime = document.getElementById("runtime");
+      runTime.textContent = `${films.runtime} Minutes`;
+      let tickets = document.querySelector("div#ticket-counter");
+      tickets.textContent = films["capacity"] - films["tickets_sold"]
+     })
+    //appending li of movies  
+        document.querySelector("ul#films").appendChild(li)
             })
         })
     }fetchMovies()
 
+  
 function baseMovie(){ fetch(baseUrl)
     .then(response => response.json())
     .then(data => {
+
+  //returning the specified element
     document.querySelector("h3#movie-title").textContent = data[0]["title"]
     document.querySelector("img#movie-poster").setAttribute("src",`${data[0]["poster"]}`)
     document.querySelector("div#showtime").textContent = data[0]["showtime"]
@@ -49,8 +49,11 @@ function baseMovie(){ fetch(baseUrl)
 }
 baseMovie()
 
+//buying tickets
 function buyTicket(){
     let button = document.querySelector("button#buy-ticket")
+
+//eventlistner on buy ticket
     button.addEventListener("click",function(){
     let currentLi = document.querySelector("div#ticket-counter")
     let number = parseInt(currentLi.textContent)
